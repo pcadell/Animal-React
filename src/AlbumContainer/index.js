b71d2f6c03d2c923df3de348c506b7c447b0abf6
Patrick Cadell <cadell.patrick@gmail.com>
2019-11-22 10:11:31 -0600

    mostly notes

--- src/AlbumContainer/index.js
+++ src/AlbumContainer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import '../App.css';
+import GenreList from './GenreList'
 
 export default class AlbumContainer extends Component {
 	constructor(){
@@ -9,8 +10,26 @@ export default class AlbumContainer extends Component {
 		this.state = {
 		albums: [],
 		genres: []
+	}}
+
+	componentDidMount(){
+		this.getAlbums();
+		this.getGenres();
 	}
+
+	getGenres = async () => {
+		try {
+			const genres = await fetch(process.env.REACT_APP_API_URL + '/api/v1/albums/genres');
+			const parsedGenres = await genres.json();
+			console.log(parsedGenres)
+			this.setState({
+				genres: parsedGenres.data
+			})
+		} catch(err) {
+			console.error(err)
+		}
 	}
+
 	getAlbums = async () => {
 		try {
 			const albums = await fetch(process.env.REACT_APP_API_URL + '/api/v1/albums/');
@@ -25,9 +44,22 @@ export default class AlbumContainer extends Component {
 	}
 	render(){
 		return(
-			<React.Fragment>
-				This is a place-holder
-			</React.Fragment>
+			<Grid
+				columns={2} 
+				divided
+				textAlign='center'
+				style={{height: '100%'}}
+				verticalAlign='top'
+				stackable
+			>
+			<Grid.Row>
+				<Grid.Column>
+					<GenreList
+					genres={this.state.genre}
+					/>
+				</Grid.Column>
+			</Grid.Row>
+			</Grid>
 			)
 	}
 
---
+++ src/GenreList/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const GenreList(props){
+
+	const genres = props.genres.map((genre) => {
+		return(
+			<div>
+				<h1 onClick={}>{genre}</a></h1>
+			</div>
+			)
+	})
+
+
+}
